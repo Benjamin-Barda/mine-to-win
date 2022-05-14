ab408fdb283b6025cdb4c2ba458f1fdd0f54f6b4
Benjamin <80880329+Benjamin-Barda@users.noreply.github.com>
2022-05-14 18:04:55 +0200

    Create JSONParser.py

---
+++ JSONParser.py
@@ -0,0 +1,36 @@
+# Reorder the JSON st coordinates are [ul-x, ul-y, width, height]
+
+import json
+
+mobList = ['Chicken', 'Cow', 'Creeper', 'Pig', 'Sheep', 'Skeleton', 'Slime', 'Spider', 'Wolf', 'Zombie', 'purge', 'written']
+
+path_to_json = '' 
+
+with open(path_to_json, 'r') as f : 
+    dic = json.loads(f.read())
+
+parsed = {}
+
+# K = Image name 
+# v = {Label : boxes}
+for image_name , d in dic.items() :
+    parsed[image_name] = {x : [] for x in mobList}
+    
+    for mob, boxes in d.items() : 
+        for box in boxes : 
+
+            x0, x1 = box[0], box[2]
+            y0, y1 = box[1], box[3]
+
+            W = abs(x0 - x1)
+            H = abs(y0 - y1)
+
+            upper_left_x = min(x0, x1)
+            upper_left_y = min(y0, y1)
+
+            parsed[image_name][mob].append([upper_left_x, upper_left_y, W, H])
+        
+            
+
+
+         
