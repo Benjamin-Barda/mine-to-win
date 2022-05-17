3df3f18d021cea28d7bdb7eb6c329ca2463b89ff
dario-loi <loi.1940849@studenti.uniroma1.it>
2022-05-17 15:07:49 +0200

    ClassLoader

--- data/ClassLoader.py
+++ data/ClassLoader.py
@@ -1,13 +1,44 @@
+
 import torch
 import pandas as pd
 from torch.utils import data
+import cv2
+import numpy as np
 
 class ClassLoader(data.Dataset):
 
 
     def __init__(self, sources = [],  transform = None):
 
-        df_iter = (pd.read_json(src) for src in sources)
+        self.transform = transform
+        df_iter = (pd.read_json(src).T for src in sources)
+        df = pd.concat(df_iter).T.drop(["purge", "written"]).T
+        print(df)
+        self.images = np.asarray(
+                [cv2.imread("imgs\\frames\\" + i) 
+                for i in df.index]
+                )
 
-        df = pd.concat(df_iter)
+        self.images = torch.ByteTensor(self.images)
+
+        self.labels = torch.ByteTensor( 2 * df["Pig"].astype(bool).astype(int) 
+                                          + df["Creeper"].astype(bool).astype(int) )
+
+        self.boundings = list(zip(df["Pig"], df["Creeper"]))
+
+        assert(len(self.images) == len(self.labels) == len(self.boundings))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img = self.images[index]
+        if self.transform:
+            img = self.transform(img)
+        
+        label = self.labels[index]
+
+        return img, label
+
+
             
---
+++ data/test.py
@@ -0,0 +1,4 @@
+from ClassLoader import ClassLoader
+
+
+x = ClassLoader(["jsons\\OUTPUT-creeper1.json"])
