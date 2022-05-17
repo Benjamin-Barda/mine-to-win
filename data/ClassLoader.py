86ec4ecb27d6b7a1af41f0cc011d610e8fcbfbfc
dario-loi <loi.1940849@studenti.uniroma1.it>
2022-05-17 18:04:47 +0200

    Loader CV

--- data/ClassLoader.py
+++ data/ClassLoader.py
@@ -7,26 +7,28 @@ import numpy as np
 
 class ClassData(data.Dataset):
 
+    def __init__(self, sources = [],  transform = None, device = "cpu"):
 
-    def __init__(self, sources = [],  transform = None):
 
         self.transform = transform
         df_iter = (pd.read_json(src).T for src in sources)
-        df = pd.concat(df_iter).T.drop(["purge", "written"]).T
+        df = pd.concat(df_iter).T.drop(["purge", "written"]).T.sample(frac=1)
         self.images = np.asarray(
                 [cv2.imread("imgs\\frames\\" + i) 
                 for i in df.index]
                 )
 
         self.images = torch.ByteTensor(self.images).permute(0,3,1,2)
-        print(self.images.shape)
-
         self.labels = torch.ByteTensor( 2 * df["Pig"].astype(bool).astype(int) 
                                           + df["Creeper"].astype(bool).astype(int) )
 
         self.boundings = list(zip(df["Pig"], df["Creeper"]))
 
         assert(len(self.images) == len(self.labels) == len(self.boundings))
+
+        if device == "cuda":
+            self.images.pin_memory()
+            self.labels.pin_memory()
 
     def __len__(self):
         return len(self.images)
@@ -44,5 +46,5 @@ class ClassData(data.Dataset):
         sample = (img, label)
         return sample
 
-
-            
+    def shape(self):
+        return self.images.shape
