61e2aaddd623196d19f609950479debd1d6c207d
dario-loi <77234595+dario-loi@users.noreply.github.com>
2022-05-30 16:53:53 +0200

    Tester changes

--- rpn_test.py
+++ rpn_test.py
@@ -14,7 +14,7 @@ SHOW = True
 bs = 1
 
 # Loading only one image
-ds = torch.load("data\\datasets\\minedata_nonull_local.dtst")
+ds = torch.load("data\\datasets\\minedata_compressed_local.dtst")
 dl = DataLoader(ds, batch_size=bs, pin_memory=True, shuffle=True)
 
 state_extractor, state_rpn = torch.load("./MineRPN_best_weights.pth", map_location=torch.device('cpu'))
@@ -24,35 +24,34 @@ extractor = BackboneCNN(is_in_rpn=True).to(("cpu"))
 extractor.load_state_dict(state_extractor)
 lossfn = RPNLoss()
 
-extractor.eval()
+for i in range(3):
+    extractor.eval()
 
-with torch.no_grad():
-    img, lbl, bounds = ds[700]
-    img = img[None, ...]
-    base_feat_map = extractor.forward(img)
-_, inDim, hh, ww, = base_feat_map.size()
+    with torch.no_grad():
+        img, lbl, bounds = ds[101]
+        img = img[None, ...]
+        base_feat_map = extractor.forward(img)
+    _, inDim, hh, ww, = base_feat_map.size()
 
-rpn = _rpn(inDim).to(("cpu"))
-rpn.load_state_dict(state_rpn)
+    rpn = _rpn(inDim).to(("cpu"))
+    rpn.load_state_dict(state_rpn)
 
-score, reg, rois_index = rpn(base_feat_map, img.shape[-2:])
+    score, reg, rois_index = rpn(base_feat_map, img.shape[-2:])
 
-img = img.permute(0, 2, 3, 1)[0, ...].numpy()
-img = np.ascontiguousarray(img)
-cv.imshow("orig img", img)
+    img = img.permute(0, 2, 3, 1)[0, ...].numpy()
+    img = np.ascontiguousarray(img)
+    cv.imshow("orig img", img)
 
-print(score.shape)
-for indx, an in enumerate(rois_index[0][0]):
-    col = (0,255,0)
-    if score[0][indx] < .5:
-        col = (0,0,255)
-        continue
-    x, y, w, h = an
+    print(score.shape)
+    for indx, an in enumerate(rois_index[0]):
+        col = (0,255,0)
+        if score[0][indx] < .5:
+            col = (0,0,255)
+        x,y,w,h = an.int()
 
-    x1,y1,x2,y2 = int(x.item() - w.item()//2), int(y.item() - h.item()//2), int(x.item() + w.item()//2), int(y.item() + h.item()//2)
+        x1,y1,x2,y2 = int(x.item() - w.item()//2), int(y.item() - h.item()//2), int(x.item() + w.item()//2), int(y.item() + h.item()//2)
+        cv.rectangle(img, (x1,y1),(x2,y2), color=col, thickness=1)
 
-    cv.rectangle(img, (x1,y1),(x2,y2), color=col, thickness=1)
-
-cv.namedWindow("img", cv.WINDOW_NORMAL)
-cv.imshow("img", img)
-cv.waitKey(0)
+    cv.namedWindow("img", cv.WINDOW_NORMAL)
+    cv.imshow("img", img)
+    cv.waitKey(0)
